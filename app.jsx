53769a0c1559b045a52970ce566d9604e2ca347c
Jonathan Hall <jonathan.edward.hall@gmail.com>
2017-02-11 16:40:41 -0500

    not sure why but the main app.jsx file was also copied into the root folder.

--- app.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Form from './src/js/components/form';
-
-ReactDOM.render(
-  <Form buttonText="Get Collection" />,
-  document.getElementsByClassName('article--main')[0]
-);
