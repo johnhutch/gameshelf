6075fc1912b0e1204d2dbe73d62e8f6775ebd840
arbitrarynoun <arbitrarynoun@gmail.com>
2017-02-12 12:29:58 -0500

    formatting and spacing tweaks with added comments for disambiguation, also added an alias to get the css import working, finally.

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,58 +3,56 @@ import path from 'path';
 module.exports = {
 
   entry: "./src/js/app.jsx",
-  
-  output: {
-  
-    path: path.resolve(__dirname, "public/"),
-  
-    filename: "app.js",
-  
-  },
+    output: {
+      path: path.resolve(__dirname, "public/"),
+      filename: "app.js",
+    },
 
-  module: {
-
-    rules: [
-
-      {
-        test: /\.jsx?$/,
-        exclude: [
-          path.resolve(__dirname, "node_modules/")
-        ],
-
-        use: [
-          "react-hot-loader", 
-          "babel-loader"
-        ]
-      },
-
-      {
-        test: /\.css$/,
-
-        use: [ 
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: "\.useable\.css$",
-
-        use: [
-          { 
-            loader: "style-loader",
-            options: {
-              useable: true
-            }
-          },
-          { 
-            loader: "css-loader" 
-          }
-        ]
+    resolve: {
+      alias: {Styles: path.resolve(__dirname, "src/css/")
       }
-    ]
-  },
+    },
 
-  devServer:{
-    contentBase: 'public'
-  }  
-}
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: [
+            path.resolve(__dirname, "node_modules/")
+          ],
+          use: [
+            "react-hot-loader", 
+            "babel-loader"
+          ]  // close use
+        },  // close test .jsx
+
+        {
+          test: /\.css$/,
+          use: [ 
+            "style-loader",
+            "css-loader"
+          ]  // close use
+        },  // close test .css
+
+        {
+          test: /\.useable\.css$/,  // not sure about the relevance of this tag anymore -sb
+          use: [
+            { 
+              loader: "style-loader"
+            },
+            {
+              loader: "css-loader",
+              options: {
+                useable: true
+              } 
+            }     // close css-loader
+          ]     // close use (line ~38)  
+        },    // close test useable.css (line ~36)
+      ]     // close rules (line ~16)
+    },    // close module (line ~15) 
+
+    devServer: {
+      contentBase: 'public'
+    },  
+
+} // close module.exports (line ~3) 
