afc9945057f834dd9a20189abc4bbc595154d4b1
arbitrarynoun <arbitrarynoun@gmail.com>
2017-02-12 06:23:27 -0500

    package.json has a few more loaders; app.jsx has a comment change that doesn't seem interesting

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from './components/Main.jsx';
 
-/*
- * Render the Main component which contains the bulk of the app's other components too.
- */
+
+// Render the Main component which contains the bulk of the app's other components too.
+
 ReactDOM.render(
   <Main />,
   document.querySelector('.container--main'),
